5a876f35cb20379821d746a9290a723b3b0afd01
gmark <mark.guo@gmail.com>
2021-09-17 13:04:33 -0400

    Initial commit

---
+++ src/test/java/dguo/dev/whatismyip/WhatismyipApplicationTests.java
@@ -0,0 +1,13 @@
+package dguo.dev.whatismyip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatismyipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
